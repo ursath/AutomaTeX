5ee105a7fe68e41e1d3be6712bb2f55b9c49433e
agussang <msanguinetti@itba.edu.ar>
2024-06-11 22:57:46 -0300

    feat: adding khash hash map to symbol table

--- src/main/c/backend/domain-specific/Table.c
+++ src/main/c/backend/domain-specific/Table.c
@@ -1,34 +1,59 @@
 #include "Table.h"
+#include "khash.h"
 
+
+typedef struct {
+	Value value;
+    ValueType type;
+} Entry;
+
+
+KHASH_MAP_INIT_STR(myhash, Entry)
+
+khash_t(myhash) * hashTable;
+
+
+void initializeTable(void){
+   hashTable = kh_init(myhash);
+}
 
 // EL CPY lo hace el usuario
-Value getValue(char * identifier, ValueType type){
-    Entry result;
-    //result = hs_get( identifier)
-    if ( result==NO_RESULT || type != result.type )
-        return NO_RESULT;
-    return result.value;
+EntryResult getValue(char * identifier, ValueType type){
+    EntryResult result = { .found=false} ;
+    khiter_t k = kh_get(myhash, hashTable, identifier);
+    if ( k==kh_end(hashTable) )
+        return result;
+    Entry entry = kh_value(hashTable,k);
+    if ( type == entry.type ) {
+        result.found=true, 
+        result.value=entry.value;
+    }
+    return result;
 }
 
 // todo 
-boolean exists(char * identifier ) {
-    //return hs_exist
-    return false; 
-}
-
-boolean exists(char * identifier, ValueType type ) {
-    Entry entry; // = hs_get(identifier);
-    return entry!=NULL && entry.type==type;
-}
+//boolean exists(char * identifier ) {
+//    //return hs_exist
+//    return false; 
+//}
+//
+//boolean exists(char * identifier, ValueType type ) {
+//    Entry entry; // = hs_get(identifier);
+//    return entry!=NULL && entry.type==type;
+//}
 
 boolean insert(char * identifier,  ValueType type, Value value ){
-    Entry * entry = malloc(sizeof(Entry));
-    entry->type = type;
-    entry->value = value;
-    
-    if( exists(identifier))
-        return false; 
-    // hs_put
+    int ret;
+    khiter_t k = kh_put(myhash, hashTable, identifier, &ret);
+    if ( ret <= 0)
+        return false;
+
+    Entry entry = {
+        .type = type,
+        .value = value
+    };
+
+    kh_value(hashTable,k) = entry;
     return true;
 }
 
--- src/main/c/backend/domain-specific/Table.h
+++ src/main/c/backend/domain-specific/Table.h
@@ -4,34 +4,35 @@
 #include "../../frontend/syntactic-analysis/AbstractSyntaxTree.h"
 
 
-enum ValueType {
+typedef enum {
     STATES,
     ALPHABET,
     TRANSITIONS,    
     AUTOMATA
-};
+} ValueType;
 
 typedef union {
     TransitionSet * transitionSet;
     SymbolSet * symbolSet;
     StateSet * stateSet;
     Automata * automata;
-} value;  
+} Value;  
 
-typedef NULL NO_RESULT;
+typedef struct{
+    Value value;
+    boolean found;
+} EntryResult;
 
-typedef struct {
-	Value value;
-    ValueType type;
-} Entry;
-
+void initializeTable(void);
 
 /**
  * @return Value que contiene ptr a lo buscado
  * es responsabilidad del cliente copiar el contenido del ptr
  */
-Value getValue(char * identifier, ValueType type);
+EntryResult getValue(char * identifier, ValueType type);
 
-boolean exists(char * identifier, ValueType type );
+//boolean exists(char * identifier, ValueType type );
 
 boolean insert(char * identifier,  ValueType type, Value value );
+
+#endif      
